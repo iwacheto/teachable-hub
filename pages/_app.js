8fdf14978ac3c505f3c08322c7fc57d178de7e09
iwacheto <ivailo@webrika.bg>
2023-10-28 13:26:21 +0300

    Move utils folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
-import '../styles/index.scss';
-// import "../utils/interceptor";
+import "../styles/index.scss";
 // import "@uiw/react-textarea-code-editor/dist.css";
-import { Provider } from 'react-redux';
-import { store } from '../store/store';
-import Notification from '../components/notification/Notification';
+import { Provider } from "react-redux";
+import { store } from "../store/store";
+import Notification from "../components/notification/Notification";
 
 function MyApp({ Component, pageProps }) {
-  return <Provider store={store}>
-    <Notification />
-    <Component {...pageProps} />
-  </Provider>
+  return (
+    <Provider store={store}>
+      <Notification />
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
